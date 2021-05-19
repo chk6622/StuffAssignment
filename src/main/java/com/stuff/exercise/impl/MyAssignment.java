97585c3a01b1164f9781f27ddf69634feb404906
xingtong <x1980p@gmail.com>
2021-05-19 15:53:20 +1200

    Implement GetNumberFromString method of MyAssignment class, and make all test cases pass.

--- src/main/java/com/stuff/exercise/impl/MyAssignment.java
+++ src/main/java/com/stuff/exercise/impl/MyAssignment.java
@@ -1,10 +1,30 @@
 package com.stuff.exercise.impl;
 
 import com.stuff.exercise.IAssignment;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MyAssignment implements IAssignment{
     @Override
     public int[] GetNumberFromString(String numbers) {
-        return new int[0];
+        int[] rArray = null;
+
+        if(numbers != null && numbers.length() > 0){
+            String[] numArray = numbers.split(",");  //split string using ','
+
+            if(numArray != null && numArray.length > 0){
+                rArray = new int[numArray.length];
+                for(int i = 0;i < numArray.length;i++){
+                    try{
+                        rArray[i] = Integer.parseInt(numArray[i]);
+                    }
+                    catch (NumberFormatException e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        return rArray;
     }
 }
