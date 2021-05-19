c5294d5994940c5207fa71d04f92529b9de2bf8a
xingtong <x1980p@gmail.com>
2021-05-19 18:08:32 +1200

    Improve MyAssignment class to let it can handle negatives.

--- src/main/java/com/stuff/exercise/impl/MyAssignment.java
+++ src/main/java/com/stuff/exercise/impl/MyAssignment.java
@@ -3,6 +3,7 @@ package com.stuff.exercise.impl;
 import com.stuff.exercise.IAssignment;
 import org.springframework.stereotype.Component;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -14,6 +15,7 @@ public class MyAssignment implements IAssignment{
 
         if(numbers != null && numbers.length() > 0){
             LinkedList<Character> charQueue = new LinkedList();
+            LinkedList<Integer> negativeNumbers = new LinkedList();
             StringBuffer buf = new StringBuffer();
 
             char[] chars = numbers.toCharArray();  //convert string to char array
@@ -25,28 +27,53 @@ public class MyAssignment implements IAssignment{
             while(!charQueue.isEmpty()){
                 char curChar = charQueue.removeFirst();
 
-                if(curChar >= '0'&& curChar <= '9'){
+                if(curChar >= '0' && curChar <= '9'){
                     buf.append(curChar);  //add number characters to the buffer
                 }
                 else if(buf.length() > 0){ //if the current character is not a number character then add the number string in the buffer to the list
                     int value = ConvertStringToIntValue(buf.toString());
+                    if(value < 0){
+                        negativeNumbers.add(value);
+                    }
                     rArray.add(value);
                     buf.delete(0,buf.length());  //clear buf
                 }
-                if((curChar == '+'||curChar == '-')){  //if the current character is '+' or '-' and the next character is number then add the current character to the buffer
+                if((curChar == '+' || curChar == '-')){  //if the current character is '+' or '-' and the next character is number then add the current character to the buffer
                     Character nextChar = charQueue.peek();
-                    if(nextChar != null && nextChar >= '0' && nextChar <= '9'){
+                    if(nextChar != null && nextChar >= '0' && nextChar <='9'){
                         buf.append(curChar);
                     }
                 }
             }
-            if(buf.length()>0){ // add the last number to the list
+            if(buf.length() > 0){ // add the last number to the list
                 int value = ConvertStringToIntValue(buf.toString());
+                if(value < 0){
+                    negativeNumbers.add(value);
+                }
                 rArray.add(value);
             }
             else{  // if the last character is not number then throw an exception
                 throw new IllegalArgumentException("input data is invalidate!");
             }
+
+            int negativeSize = negativeNumbers.size();
+            if(negativeSize == 1){
+                throw new IllegalArgumentException("negatives not allowed!");
+            }
+            else if(negativeSize > 1){
+                StringBuffer msgBuf = new StringBuffer("negatives not allowed:");
+                Iterator<Integer> iterator = negativeNumbers.iterator();
+                while( iterator.hasNext() ){
+                    Integer negativeNumber = iterator.next();
+                    msgBuf.append(negativeNumber);
+                    if(iterator.hasNext()){
+                        msgBuf.append(",");
+                    }
+                }
+                throw new IllegalArgumentException(msgBuf.toString());
+            }
+
+
         }
 
         return rArray.stream().mapToInt(Integer::intValue).toArray();
@@ -58,7 +85,7 @@ public class MyAssignment implements IAssignment{
      * @return it returns the number that is String type, default value is 0
      * */
     private int ConvertStringToIntValue(String number) {
-        int value=0;
+        int value = 0;
         if(number.length() > 0){
             try{
                 value = Integer.parseInt(number);
